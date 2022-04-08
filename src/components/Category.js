1169f65b5d254e7edb3c5e9a768fd4eb3d0a85ce
ptrlgj <piotr.ligaj24@gmail.com>
2022-04-08 18:23:57 +0200

    styled navlinks

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,34 +1,65 @@
 import React from 'react'
 import {FaPizzaSlice, FaHamburger} from 'react-icons/fa'
 import {GiNoodles, GiChopsticks} from 'react-icons/gi'
+import { NavLink } from 'react-router-dom'
 import styled from 'styled-components'
 
 function Category() {
   return (
     <List>
-        <a>
+        <Slink to={'/cuisine/italian'}>
             <FaPizzaSlice />
-            <p>Italian</p>
-        </a>
-        <a>
+            <h4>Italian</h4>
+        </Slink>
+        <Slink to={'/cuisine/american'}>
             <FaHamburger />
-            <p>American</p>
-        </a>
-        <a>
+            <h4>American</h4>
+        </Slink>
+        <Slink to={'/cuisine/thai'}>
             <GiNoodles />
-            <p>Thai</p>
-        </a>
-        <a>
+            <h4>Thai</h4>
+        </Slink>
+        <Slink to={'/cuisine/japanese'}>
             <GiChopsticks />
-            <p>Japanese</p>
-        </a>
+            <h4>Japanese</h4>
+        </Slink>
     </List>
   )
 }
 
 const List = styled.div`
     display: flex;
-    justify-content: center;
+    justify-contizzaSliceent: center;
     padding: 1rem;    
+`
+const Slink = styled(NavLink)`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    margin-right: 2rem;
+    text-decoration: none;
+    background: linear-gradient(35deg, #494949, #313131);
+    width: 6rem;
+    height: 6rem;
+    cursor: pointer;
+    transform: scale(0.8);
+
+    h4{
+        color: white;
+        font-size: 0.8rem;
+    }
+    svg {
+        color: white;
+        font-size: 1.5rem;
+    }
+    &.active{
+        background: linear-gradient(to right, #f27121, #e94057);
+
+        svg{
+            color: white;
+        }
+    }
 `
 export default Category
