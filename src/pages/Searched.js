a5397b0701ea143148a20d2b250dfd8ea5e2c1e9
ptrlgj <piotr.ligaj24@gmail.com>
2022-04-09 11:35:20 +0200

    fetching searched term and rendering its result

--- src/pages/Searched.js
+++ src/pages/Searched.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {useParams} from 'react-router-dom'
+import styled from 'styled-components'
 
 function Searched() {
-  return (
-    <div>Searched</div>
-  )
-}
+    const params = useParams()
+    const [foundRecipes, setFoundRecipes] = useState([])
 
+    useEffect(()=>{
+        getSearched(params.search);
+        console.log("change")
+    },[params.search])
+    const getSearched = async searchedTerm => {
+        const response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&titleMatch=${searchedTerm}`);
+        const data = await response.json();
+        setFoundRecipes(data.results);
+    }
+    return (
+    <Grid>
+        {
+            foundRecipes.map(recipe=>{
+                return (
+                    <Card key={recipe.id}>
+                        <img src={recipe.image} alt={recipe.title} />
+                        <h4>{recipe.title}</h4>
+                    </Card>
+                )
+            })
+        }
+    </Grid>
+    )
+}
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+  grid-gap: 3rem;
+ `
+ const Card = styled.div`
+  img {
+    width: 100%;
+    border-radius: 2rem;
+  }
+  a {
+    text-decoration: none;
+  }
+  h4{
+    text-align: center;
+    padding: 1rem
+  }
+ `
 export default Searched
