c882a8533bff2e98a216e6b9b06d87fbf23086fc
ptrlgj <piotr.ligaj24@gmail.com>
2022-04-09 13:16:15 +0200

    logo with link to homepage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import Pages from './pages/Pages'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Link } from 'react-router-dom'
 import './App.css'
 import Category from './components/Category'
 import Search from './components/Search'
+import styled from 'styled-components'
+import {GiKnifeFork} from 'react-icons/gi'
 function App() {
   return (
     <div className='App'>
       <BrowserRouter>
+        <Nav>
+          <Logo to="/">
+            <GiKnifeFork />
+            recipe-app
+          </Logo>
+        </Nav>
         <Category/>
         <Search />
         <Pages />
@@ -15,5 +23,18 @@ function App() {
     </div>
   )
 }
-
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  font-family: 'Lobster Two', cursive;
+  `
+const Nav = styled.nav`
+  padding: 4rem 0rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  svg{
+    font-size: 2rem;
+  }`
 export default App
