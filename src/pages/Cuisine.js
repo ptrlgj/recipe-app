f188e9aa419ce3b1bba7558e4ca5aa099d1555f4
Piotr Ligaj <piotr.ligaj24@gmail.com>
2022-04-29 15:37:59 +0200

    import motiom from framer-motion

--- src/pages/Cuisine.js
+++ src/pages/Cuisine.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import {useParams, Link} from 'react-router-dom';
 import styled from 'styled-components'
+import {motion} from 'framer-motion'
 
 function Cuisine() {
     const [cuisine, setCuisine] = useState([]);
